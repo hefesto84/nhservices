8994bf81d018eaf5609ddce98aee251beefa5f72
Dani Santeugini <dani@ubiqua.es>
2015-11-11 10:50:55 +0100

    0.0.0.0

---
+++ src/main/java/es/ubiqua/nhservices/IndexAction.java
@@ -0,0 +1,13 @@
+package es.ubiqua.nhservices;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport {
+
+	private static final long serialVersionUID = 5908272978483196633L;
+
+	public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+}
